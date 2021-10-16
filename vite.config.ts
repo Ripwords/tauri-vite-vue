3114bff55c3727e5532f18f80e42045ee7a01ff3
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-10-16 23:21:39 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
+import IconsResolver from 'unplugin-icons/resolver'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Icons({}),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: 'i'
+        }),
+        NaiveUiResolver()
+      ]
+    }),
+  ]
+})
